3b7c43d317886acfbf24752e5c4b58b93091cad4
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-08-27 14:26:30 +0530

    Cloud Configuration working

--- eureka-service/src/main/java/io/base/service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/io/base/service/EurekaServiceApplication.java
@@ -2,8 +2,12 @@ package io.base.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableConfigServer
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
@@ -11,4 +15,5 @@ public class EurekaServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
+
 }
