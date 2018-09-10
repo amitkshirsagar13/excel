114ca2a7fd2752f64409ae46a7f352f95bea6d8b
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-09-10 21:51:19 +0530

    Working oauth2

--- zuul-gateway/src/main/java/io/base/zuul/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/io/base/zuul/ZuulGatewayApplication.java
@@ -2,9 +2,11 @@ package io.base.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableOAuth2Sso
 @EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
---
+++ zuul-gateway/src/main/java/io/base/zuul/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package io.base.zuul.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@Order(-10)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
+				.httpBasic().disable();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("root").password("password").roles("USER");
+	}
+
+}
